ad9d47a88ba80b7012e023c405299d851a8c9c0e
Nik Silver <nik@niksilver.com>
2025-01-11 18:00:57 +0000

    Port set by parameter

--- clockabout.lua
+++ clockabout.lua
@@ -5,21 +5,48 @@
 
 function init()
   g = {
-    devices = {}, -- Container for connected midi devices and their data
+    devices = {}, -- Container for connected midi devices and their data.
                   -- A table with keys: connection, name
-    vport = 1,  -- MIDI clock out
+                  -- Also allows a 0-indexed "none" device
+    vport = 0,       -- MIDI clock out vport (int >= 1), or 0
     key3_hold = false,
     random_note = math.random(48,72),
   }
 
-  -- Query MIDI vports, connect and collect info
+  -- Query MIDI vports, connect, collect info, switch off norns's own clock out.
+  -- Also add a device 0, which is "none".
 
+  local names = {}
   for i = 1,#midi.vports do
     local conn = midi.connect(i)
+    local name = "port "..i..": "..util.trim_string_to_width(conn.name,80)
+
     g.devices[i] = {
       connection = conn,
-      name = "port "..i..": "..util.trim_string_to_width(conn.name,80),
+      name = name,
     }
+    table.insert(names, name)
+
+    params:set("clock_midi_out_"..i, 0)
+  end
+
+  g.devices[0] = {
+    connection = null,
+    name = "none",
+  }
+  names[0] = "none"
+
+  -- Define a parameter for the out vport. Make it 1 if possible, but
+  -- beware there may be no devices available.
+
+  params:add_option("clockabout_vport", "Port", names, g.vport)
+  params:set_action("clockabout_vport", function(i)
+    g.vport = i
+    -- Temporarily use norns's own MIDI clock
+    params:set("clock_midi_out_"..i, 1)
+  end)
+  if g.devices[1] then
+    params:set("clockabout_vport", 1)
   end
 
 end
@@ -50,13 +77,11 @@ end
 function key(n,z)
   if n == 3 then
     if z == 1 then
-      local vport = target_vport()
-      g.devices[vport].connection:note_on(g.random_note) -- defaults to velocity 100 on ch 1
+      g.devices[g.vport].connection:note_on(g.random_note) -- defaults to velocity 100 on ch 1
       g.key3_hold = true
       redraw()
     elseif z == 0 then
-      local vport = target_vport()
-      g.devices[vport].connection:note_off(g.random_note)
+      g.devices[g.vport].connection:note_off(g.random_note)
       g.random_note = math.random(50,70)
       g.key3_hold = false
       redraw()
@@ -67,10 +92,8 @@ end
 function redraw()
   screen.clear()
 
-  local vport = target_vport()
-  local name = vport and g.devices[vport].name or "none"
   screen.move(0,10)
-  screen.text(name)
+  screen.text(g.devices[g.vport].name)
 
   screen.move(0,20)
   screen.text("bpm (E3): " .. params:string("clock_tempo"))
