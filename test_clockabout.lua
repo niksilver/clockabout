8a71989649822fb63a878c94abc209eeac6cb637
Nik Silver <nik@niksilver.com>
2025-01-13 10:13:43 +0000

    Add test with different beats per bar

--- test_clockabout.lua
+++ test_clockabout.lua
@@ -30,6 +30,19 @@ function test_calc_interval_60_bpm_in_middle_of_bar()
     lu.assertAlmostEquals( calc_interval(), pulses_per_beat / g.PARTS_PQN, 0.01 )
 end
 
+function test_calc_interval_60_bpm_in_middle_of_bar_3_beats_per_bar()
+    g = init_globals({
+      BEATS_PB = 3,
+      bpm = 60,
+      pulse_num = 17,
+      pulse_total = 16 + 96,
+    })
+
+    local pulses_per_beat = (60 / g.bpm) / 24 * g.BEATS_PB
+
+    lu.assertAlmostEquals( calc_interval(), pulses_per_beat / g.PARTS_PQN, 0.01 )
+end
+
 function test_calc_interval_120_bpm()
     g = init_globals({
       bpm = 120,
