b5f6f5a66c3901d9a4393b9930a1f1098127e5a2
Nik Silver <nik@niksilver.com>
2025-01-14 10:28:25 +0000

    Push script reads .gitignore file.

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ FROM_DIR=.
 TO_DIR=we@norns.lan:/home/we/dust/code/clockabout
 rsync --recursive --delete --itemize-changes \
     --exclude=.git \
-    --exclude='*.swp' \
     --exclude=push.sh \
+    --exclude-from=.gitignore \
     "$FROM_DIR" "$TO_DIR"
