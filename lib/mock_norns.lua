7be1c4d009433b1bfb4db605441f1b5d5dc61bdb
Nik Silver <nik@niksilver.com>
2025-01-25 10:06:31 +0000

    Start to add mocks to test with metros

---
+++ lib/mock_norns.lua
@@ -0,0 +1,31 @@
+-- Mocking up _norns C functions, for testing purposes.
+
+_norns = {}
+
+-- In reality a C function.
+-- @tparam int id  Metro id (number).
+-- @tparam number time  Gap between clicks.
+--
+_norns.metro_start = function(id, time, count, init_stage)
+end
+
+-- In reality a C function.
+-- @tparam int id  Metro id (number).
+--
+_norns.metro_stop = function(id)
+end
+
+-- From the metro module:
+-- NB: metro time isn't applied until the next wakeup.
+-- this is true even if you are setting time from the metro callback;
+-- metro has already gone to sleep when lua main thread gets
+-- if you need a fully dynamic metro, re-schedule on the wakeup
+--
+-- @tparam int id  Metro id (number)
+-- @tparam number time  Gap between clicks.
+_norns.metro_set_time = function(id, time)
+end
+
+-- Callback function to be set by the metro moduule.
+--
+_norns.metro = nil
